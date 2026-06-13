d8e647487c544eefe8274cf31fe602af37e734ca
catksbo <uniquemahadi4@gmail.com>
2026-06-13 15:06:29 +0600

    feat: implement user registration with bcrypt hashing and JWT token generation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,40 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Res} from '@nestjs/common';
+import type { Response } from 'express';
+import {UnauthorizedException} from '@nestjs/common';
 import { AuthService } from './auth.service';
+import bcrypt from "bcrypt";
 
 @Controller('auth')
 export class AuthController {
   constructor(private  authService: AuthService) {}
 
-  @Post('login')
-  async loginUser(@Body() body: { email: string; password: string }) {
-    return this.authService.loginUser(body.email, body.password);
+  // @Post('login')
+  // async loginUser(@Body() body: { email: string; password: string }) {
+  //   return this.authService.loginUser(body.email, body.password);
+  // }
+
+  @Post('register')
+  async registerUser(@Body() body: { email: string; password: string }, @Res({ passthrough: true }) res: Response) {
+    if (!body.email || !body.password) {
+      throw new Error('Email and password are required');
+    } 
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(body.password, salt);
+    const token = await this.authService.registerUser(body.email, hashedPassword);
+    if (!token) {
+    throw new UnauthorizedException();
+    };
+
+    res.cookie("access_token", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "lax",
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  });
+    return { message: 'User registered successfully', token };
   }
+
+
 }
+
+
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,18 +1,38 @@
+import { JwtService } from '@nestjs/jwt';
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
-  async loginUser(userEmail: string, userPassword: string) {
-    await this.prisma.user.create({
+  constructor(private prisma: PrismaService, 
+    private jwtService: JwtService
+  ) {}
+  // async loginUser(userEmail: string, userPassword: string) {
+  //   await this.prisma.user.create({
+  //     data: {
+  //       email: userEmail,
+  //       password: userPassword
+  //     }
+  //   }) 
+  // }
+  async registerUser(userEmail: string, userPassword: string) {
+    const existsUser = await this.prisma.user.findUnique({
+      where: {
+        email: userEmail
+      }
+    });
+    if (existsUser) {
+      throw new Error('User already exists');
+    }
+    const user = await this.prisma.user.create({
       data: {
         email: userEmail,
         password: userPassword
       }
-      
-    })
-    console.log(userEmail, userPassword); 
+    });
+    const payload = { sub: user.id };
+    const token = this.jwtService.sign(payload);
+    return token;
   }
 
 }
